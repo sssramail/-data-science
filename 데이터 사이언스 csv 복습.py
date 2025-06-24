7e49b8c080a838b1f54c6fd5c37c839c9f34d8c3
sssramail <zion1nice@gmail.com>
2025-06-24 18:55:29 +0900

    Add files via upload

---
+++ 데이터 사이언스 csv 복습.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+#import random
+import csv
+
+f = open("z.csv","r",encoding = "utf-8")
+data = read(f)
+
+
+
+#List = []
+
+#for _ in range(10):
+#    List.append(random.randint(1,10))
+    
+#print(List)
+#print(List.count(7))
+#List.sort()
+#print(List)
+#List.reverse()
+#print(List)
+
+#plt.title("plotting")
+
+#plt.plot([6,2,7,3],[10,40,32,45],color = "red",label = "asc")
+
+#plt.plot([6,2,7,3],[45,32,40,10],color = "blue",label = "besc")
+
+#plt.legend()
+
+#plt.show()
+
+
+
