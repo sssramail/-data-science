53d90c62963f631a374624fb3512c2f2d127d182
sssramail <zion1nice@gmail.com>
2025-07-08 18:56:11 +0900

    Add files via upload

---
+++ 데이터 사이언스 5주차 보강(상상).py
@@ -0,0 +1,24 @@
+import numpy as np
+
+'''
+a = ['1','2','3','4']
+b = np.array(a)
+c = np.array(a,dtype = int)
+
+print(b)
+print(c)
+
+
+c = np.arange(0,10,2)
+print(c)
+'''
+
+a = np.random.random()
+b = np.random.random(5)
+c = np.random.randint(0,101)
+d = np.random.randint(0,101,5)
+
+print(a)
+print(b)
+print(c)
+print(d)
