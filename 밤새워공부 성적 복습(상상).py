05962d02284b695ee2dd260645429c4cd6dfc99a
sssramail <zion1nice@gmail.com>
2025-08-05 18:59:51 +0900

    Add files via upload

---
+++ 밤새워공부 성적 복습(상상).py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("sleeping_hours.csv",encoding = "euc-kr",header = [0,1],index_col = [0,1])
+df = df.T
+df = df.rename_axis(index = ["연도","수면시간"])
+
+times = ["5시간 미만","5~6시간","6~7시간","7~8시간","8~9시간","9시간 이상"]
+
+high_2018 = df["학업성적"].loc["2018","상"]
+middle_2018 = df["학업성적"].loc["2018","중"]
+low_2018 = df["학업성적"].loc["2018","하"]
+
+
+plt.rc("font",family = "Malgun Gothic")
+plt.title("2018년 학업 성적별 수면 시간")
+plt.plot(times,high_2018,label = "상",color = "skyblue")
+plt.plot(times,middle_2018,label = "중",color = "yellow")
+plt.plot(times,low_2018,label = "하",color = "green")
+plt.legend()
+plt.show()
+
+high_2019 = df["학업성적"].loc["2019","상"]
+middle_2019 = df["학업성적"].loc["2019","중"]
+low_2019 = df["학업성적"].loc["2019","하"]
+
+
+plt.rc("font",family = "Malgun Gothic")
+plt.title("2019년 학업 성적별 수면 시간")
+plt.plot(times,high_2019,label = "상",color = "skyblue")
+plt.plot(times,middle_2019,label = "중",color = "yellow")
+plt.plot(times,low_2019,label = "하",color = "green")
+plt.legend()
+plt.show()
